303a2c9911372975240c3a48585f75df0a187df4
mvp18 <soumava2016@gmail.com>
2020-05-30 03:04:16 +0530

    lstm on schluter features initial commit

---
+++ lstm_scnn_feat/main.py
@@ -0,0 +1,104 @@
+import os
+import shutil
+import numpy as np
+import pandas as pd
+import time
+import sys
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.activations import softmax
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+import h5py
+import argparse
+
+from load_data import *
+from model import *
+from config_cnn import *
+from utils import test
+
+# set gpu number 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-model', '--model_type', help="teacher/student", default='teacher', type=str)
+parser.add_argument('-bs', '--batch_size', help="batch size used for training", default=64, type=int)
+parser.add_argument('-lr', '--learning_rate', help="learning rate for adam", default=1e-4, type=float)
+parser.add_argument('-e', '--num_epochs', help="number of epochs to run", default=50, type=int)
+parser.add_argument('-es', '--early_stop', default=7, type=int)
+parser.add_argument('-rd_lr', '--reduce_lr', default=5, type=int)
+parser.add_argument('-seed', '--rand_seed', default=0, type=int)
+
+args = parser.parse_args()
+
+np.random.seed(args.rand_seed)
+
+if args.model_type=='teacher': model = Leglaive_RNN(timesteps=CNN_INPUT_SIZE)
+elif args.model_type=='student': model = RNN_small(timesteps=CNN_INPUT_SIZE)
+else:
+    print('Invalid model type specified!')
+    sys.exit()
+
+opt = Adam(lr=args.learning_rate)
+
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['acc'])
+print(model.summary())
+
+timestampTime = time.strftime("%H%M%S")
+timestampDate = time.strftime("%d%m%Y")
+timestampLaunch = timestampDate + '_' + timestampTime
+
+wts_dir = './weights_'+timestampLaunch+'/'
+if not os.path.exists(wts_dir): os.makedirs(wts_dir)
+
+score_string = 'val_acc-{val_acc:.4f}_'+args.model_type+'_tr_acc-{acc:.4f}_bestEp-{epoch:02d}'
+model_save_name = wts_dir+score_string+'_bs-'+str(args.batch_size)+'_lr-'+str(args.learning_rate)+'.h5'
+
+checkpoint = ModelCheckpoint(filepath=model_save_name, monitor='val_acc', verbose=1, save_weights_only=False, save_best_only=True, mode='auto')
+    
+earlyStopping = EarlyStopping(monitor='val_acc', patience=args.early_stop, verbose=1, mode='auto')
+    
+reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.8, patience=args.reduce_lr, verbose=1, min_lr=1e-8)
+
+X_tr, y_train = load_xy_data(None, MEL_JAMENDO_DIR, JAMENDO_LABEL_DIR, 'train')
+Y_tr = to_categorical(y_train, 2)
+
+X_val, y_val = load_xy_data(None, MEL_JAMENDO_DIR, JAMENDO_LABEL_DIR, 'valid')
+Y_val = to_categorical(y_val, 2)
+
+print("Train Data Shape", X_tr.shape, Y_tr.shape)
+print("Val Data Shape", X_val.shape, Y_val.shape)
+
+history = model.fit(X_tr, Y_tr, batch_size=args.batch_size, epochs=args.num_epochs, shuffle=True, validation_data=(X_val, Y_val), 
+                    callbacks=[checkpoint, earlyStopping, reduce_lr])
+
+tr_loss = history.history['loss']
+val_loss = history.history['val_loss']
+tr_acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+idx, best_val_acc = np.argmax(val_acc), np.max(val_acc)
+corr_tr_acc = tr_acc[idx]
+
+df_save = pd.DataFrame({'tr_loss':tr_loss, 'val_loss':val_loss, 'tr_acc':tr_acc, 'val_acc':val_acc})
+
+best_model_name = 'val_acc-'+'{0:.4f}_'.format(best_val_acc)+args.model_type+'_tr_acc-'+'{0:.4f}'.format(corr_tr_acc)+\
+				  '_bestEp-'+'{:02d}'.format(idx+1)+'_bs-'+str(args.batch_size)+'_lr-'+str(args.learning_rate)+'.h5'
+
+save_path = './results/'+args.model_type+'/'
+if not os.path.exists(save_path): os.makedirs(save_path)
+
+scores = test(best_model_name, args.model_type, wts_dir, args)
+suffix = best_model_name[:-3]+'_acc-{0:.4f}'.format(scores[0])+'_pr-{0:.4f}'.format(scores[1])+'_re-{0:.4f}'.format(scores[2])+\
+		'_f1-{0:.4f}'.format(scores[3])+'_fp-{0:.4f}'.format(scores[4])+'_fn-{0:.4f}'.format(scores[5])
+
+df_save.to_csv(open(save_path + suffix + '.csv', 'w'))
+
+shutil.move(wts_dir+best_model_name, './'+best_model_name)
+
+if os.path.exists(wts_dir): shutil.rmtree(wts_dir)
+
+print("Finished!")
---
+++ lstm_scnn_feat/utils.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.losses import categorical_crossentropy as logloss
+from tensorflow.keras.losses import kullback_leibler_divergence
+from tensorflow.keras.metrics import categorical_accuracy
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+import argparse
+import h5py
+
+from load_data import *
+from model import *
+from config_cnn import *
+
+def acc(y_true, y_pred):
+	y_true_hard = y_true[:, :2]
+	y_pred_hard = y_pred[:, :2]
+	return categorical_accuracy(y_true_hard, y_pred_hard)
+
+def categorical_crossentropy(y_true, y_pred):
+	y_true_hard = y_true[:, :2]
+	y_pred_hard = y_pred[:, :2]
+	return logloss(y_true_hard, y_pred_hard)
+
+def kld_loss(y_true, y_pred):     
+	y_true_softs = y_true[:, 2:]
+	y_pred_softs = y_pred[:, 2:]
+	return kullback_leibler_divergence(y_true_softs, y_pred_softs)
+
+def kd_loss(alpha, temperature):
+
+	def custom_loss(y_true, y_pred):
+
+		y_true, y_true_softs = y_true[:, :2], y_true[:, 2:]
+		y_pred, y_pred_softs = y_pred[:, :2], y_pred[:, 2:]
+		
+		loss = (1-alpha)*logloss(y_true, y_pred) + alpha*(temperature**2)*kullback_leibler_divergence(y_true_softs, y_pred_softs)
+	
+		return loss
+
+	return custom_loss
+
+def sample_scores(loaded_model, model_type, song):
+
+	x_test, y_test = load_xy_data(song, MEL_JAMENDO_DIR, JAMENDO_LABEL_DIR)
+
+	y_pred = loaded_model.predict(x_test, verbose=1)
+
+	if model_type=='kd': y_pred = y_pred[:, :2]
+
+	y_pred = np.argmax(y_pred, axis=1)
+
+	y_pred = y_pred.reshape(-1).astype(int)
+	y_test = y_test.reshape(-1).astype(int)
+
+	accuracy_single = (len(y_test) - np.sum(np.abs(y_pred - y_test)))/ len(y_test)
+	f1 = f1_score(y_test, y_pred, average='binary')
+	pr = precision_score(y_test, y_pred, average='binary')
+	re = recall_score(y_test, y_pred, average='binary')
+	
+	print('\nSample Scores...\n')
+	print('Accuracy: ' + str(accuracy_single))
+	print('Precision: ', pr)
+	print('Recall: ', re)
+	print('F1-score: ', f1)
+
+	del x_test
+
+	return y_pred, y_test
+
+def test(model_name, model_type, wts_dir, args):
+
+	if model_type=='kd':
+		model = RNN_small(timesteps=RNN_INPUT_SIZE)
+		model.pop()
+		model_logits = model.layers[-1].output
+		model_logits_T = Lambda(lambda x: x/args.temperature)(model_logits)
+		probs_T = Softmax(axis=1)(model_logits_T)
+		probs_1 = Softmax(axis=1)(model_logits)
+		output = Concatenate()([probs_1, probs_T])
+		model = Model(inputs=model.input, outputs=output)
+
+		model.load_weights(wts_dir+model_name)
+	else:
+		model = tf.keras.models.load_model(wts_dir+model_name)
+
+	print(model.summary())
+
+	list_of_songs = os.listdir(MEL_JAMENDO_DIR + 'test')
+
+	y_preds = []
+	y_tests = []
+
+	for song in list_of_songs:
+		
+		y_pred, y_test = sample_scores(model, model_type, song)
+
+		for i in range(len(y_pred)):
+			y_preds.append(y_pred[i])
+			y_tests.append(y_test[i])
+
+	# convert list to np array 
+	y_preds = np.array(y_preds)
+	y_tests = np.array(y_tests)
+
+	# calculate scores 
+	acc = (len(y_tests) - np.sum(np.abs(y_preds - y_tests))) / float(len(y_tests))
+
+	f1 = f1_score(y_tests, y_preds, average='binary')
+	pr = precision_score(y_tests, y_preds, average='binary')
+	re = recall_score(y_tests, y_preds, average='binary')
+
+	tn, fp, fn, tp = confusion_matrix(y_tests, y_preds).ravel()
+	fp_rate = fp / (fp + tn)
+	fn_rate = fn / (fn + tp)
+
+	print("TEST SCORES OVERALL\n")
+	print('Acc %.4f' % acc)
+	print('Precision %.4f' % pr)
+	print('Recall %.4f' % re)
+	print('F1-score %.4f' % f1)
+	print('fp rate', fp_rate, 'fn_rate', fn_rate)
+
+	scores = [acc, pr, re, f1, fp_rate, fn_rate]
+
+	return scores
