97e63f17092860c976fe76a48a19bf3a91148837
mvp18 <soumava2016@gmail.com>
2020-06-02 13:21:54 +0530

    student student runs for enkd scnn feat

--- enkd_scnn_feat_student-lstm/sh_generator/kd_runs_student_model.py
+++ enkd_scnn_feat_student-lstm/sh_generator/kd_runs_student_model.py
@@ -3,8 +3,8 @@ import numpy as np
 jobstr = "python3 main_kd.py -e 50 -es 7 -rd_lr 5 -bs 64 -lr 1e-4 -model student "
 jobs = []
 
-for temp in [2, 5, 7, 10, 20]:
-	for alpha in np.arange(0.1, 0.91, 0.1):
+for temp in [2, 5, 10, 20]:
+	for alpha in np.arange(0.1, 0.91, 0.2):
 		for comb in ['am', 'gm']:
 			jobs.append(jobstr + 
 			' '.join([
